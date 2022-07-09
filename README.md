8fc16209246aa73453e96bbfc698dcc071f7e592
marcosfitzsimons <marcosfitzsimons@gmail.com>
2022-07-08 23:45:37 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+### In this project only use the css framework Tailwind Css.
+
+### Tailwind CSS - Easy to setup? by A shot of code, great youtube resource for begginers:
+
+        https://www.youtube.com/watch?v=RUVLnECJNrw
+
+### At the moment I Write this line of code:
+
+    I only have one problem to vinculate my css custom variables with tailwind functionality.
+    I can fix this easily, following the steps of this docs:
+
+### https://tailwindcss.com/docs/customizing-colors#using-css-variables
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    fontFamily: {
+      'rubik': ['"Rubik"', 'sans-serif'] // Ensure fonts with spaces have " " surrounding it.
+    },
+    colors: {
+      'pale-violet': 'hsl(var(--pale-violet) / 1)',
+      'moderat-violet': 'hsl(var(--moderat-violet) / .8)',
+      'desaturate-dark-violet': 'hsl(var(--desaturate-dark-violet) / 1)',
+      'grayish-blue': 'hsl(var(--grayish-blue) / 1)',
+      'very-dark-desaturated-violet': 'hsl(var(--very-dark-desaturated-violet) / 1)',
+      'dark-grayish-violet': 'hsl(var(--dark-grayish-violet) / 1)',
+      'lightmagenta': 'hsl(var(--lightmagenta) / 1)',
+      'lightviolet': 'hsl(var(--lightviolet) / 1)',
+      'lightgray': 'hsl(var(--lightgray) / .9)',
+      'white': 'hsl(var(--white) / 1)',
+      'light-grayish-violet': 'hsl(var(--light-grayish-violet) / 1)',
+      'very-dark-desaturated-violet': 'hsl(var(--very-dark-desaturated-violet) / 1)',
+      'very-light-magenta': 'hsl(var(--very-light-magenta) / 1)',
+      'bg-chat': '#E4DCE8'
+    },
+  },
+  plugins: [],
+}
